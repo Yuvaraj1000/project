7f478286c922e4834038b2619c1cfc29c151fde0
kollepara yuvaraj <kolleparayuvaraj@kolleparas-MacBook-Air.local>
2023-05-24 14:38:35 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require( "express"); const app = express();
+
+app.get('/', (req, res) => {
+
+res.end(JSON.stringify({ message: 'NodeJs App Running on Amazon ECS Fargate' }));
+
+})
+
+app.listen(3000, () => { console.log("server connected");
+
+});
